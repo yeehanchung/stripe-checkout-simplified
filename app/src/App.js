33b7fcf488b90e781629e4de1bc2a7a9c7291f65
Yee Han <cyh0011215@gmail.com>
2022-02-21 23:21:57 +0800

    refactor(app): more features

--- app/src/App.js
+++ app/src/App.js
@@ -10,9 +10,10 @@ const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 function App() {
 
   const [errorMsg, SetErrorMsg] = useState("");
+  const [quantity, setQuantity] = useState(1);
   const [isLoading, setLoading] = useState(false);
 
-  const handleClick = async (event) => {
+  const handleOnClickCheckout = async (event) => {
     setLoading(true);
 
     // Check for network connection
@@ -34,30 +35,51 @@ function App() {
     setLoading(false);
   };
 
+  const decrement = () => {
+    setQuantity(prevQty => {
+      if(prevQty > 1) {
+        return prevQty - 1;
+      }
+      return 1;
+    })
+  }
+
+  const increment = () => {
+    setQuantity(prevQty => prevQty + 1);
+  }
+
+  async function redirectToCheckout(stripe) {
+    await stripe.redirectToCheckout({
+      lineItems: [{
+        price: 'price_1ItByuGISOPmwDos8XvYEIBP',
+        quantity: quantity,
+      }],
+      mode: 'subscription',
+      successUrl: 'https://example.com/success',
+      cancelUrl: 'https://example.com/cancel',
+    })
+  }
+
   return (
     <div className={styles.ctn}>
+      <h2>Pro Plus Plan</h2>
+      <h4>MYR {quantity * 15} per year</h4>
 
-      <button onClick={handleClick} disabled={isLoading} className={styles.buttonCta}>
+      <span className={styles.description}>30 days free</span>
+      <span className={styles.quantity}>Quantity: {quantity}</span>
+
+      <div className={styles.buttonGroup}>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+
+      <button onClick={handleOnClickCheckout} disabled={isLoading} className={styles.buttonCta}>
         {isLoading? "Loading..." : "Checkout"}
       </button>
 
       {errorMsg && <p>{errorMsg}</p>}
-
     </div>
   );
-}
-
-async function redirectToCheckout(stripe) {
-
-  await stripe.redirectToCheckout({
-    lineItems: [{
-      price: 'price_1ItByuGISOPmwDos8XvYEIBP',
-      quantity: 1,
-    }],
-    mode: 'subscription',
-    successUrl: 'https://example.com/success',
-    cancelUrl: 'https://example.com/cancel',
-  })
 }
 
 export default App;
